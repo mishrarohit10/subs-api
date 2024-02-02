095eb183eacf1c6376975dafc88518687efe3cfe
mishrarohit10 <mishrarohit316x@gmail.com>
2024-02-02 12:24:58 +0530

    added express middleware

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-mongoose.connect();
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection
+
+app.use(express.json())
 
 db.on('error', (error) => {
     console.error(error);
