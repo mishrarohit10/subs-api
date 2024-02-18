aabb23e75fa527c68fda58d46b32cd40d38b949a
mishrarohit10 <mishrarohit316x@gmail.com>
2024-02-19 03:17:50 +0530

    fixed status codes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -26,7 +26,7 @@ router.post('/', async(req,res) => {
     try {
         const newSubscriber = await Subscriber.save()
     } catch(err) {
-        res.status(500).json({message:err.message})
+        res.status(201).json({message:err.message})
     }
 })
 // update one
